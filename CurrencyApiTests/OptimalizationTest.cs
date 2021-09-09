b079da13e37b16be016764f043ca5303ff4f85a7
JBMErykTaszarek <eryk.taszarek@gmail.com>
2021-09-09 21:40:09 +0200

    Added Testing

---
+++ CurrencyApiTests/OptimalizationTest.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CurrencyApiTests
+{
+    public class OptimalizationTest : TestBase
+    {
+        [Fact]
+        public async Task OneRateSpeedTest()
+        {
+            string path = Path.Combine(Environment.CurrentDirectory, @"..\..\..\SpeedTestResaults\", DateTime.Now.ToString("dd-MM-yyyy-HH-mm-ss") + "-SpeedTest.txt");
+            var serializedQueryObject = JsonConvert.SerializeObject(createContent("PLN", "EUR", "2018-09-01", "2018-09-01"));
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            var response = await _client.PostAsync("/GetCurrencyRates", new StringContent(serializedQueryObject, Encoding.UTF8, "application/json"));
+            sw.Stop();
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            if (!File.Exists(path))
+            {
+                using (FileStream fs = File.Create(path)) ;
+            }
+            using (StreamWriter writer = File.AppendText(path))
+            {
+                writer.WriteLine($"Getting One rate last: {sw.Elapsed}");
+
+            }
+        }
+
+    }
+}
---
+++ CurrencyApiTests/TestBase.cs
@@ -0,0 +1,49 @@
+﻿using CurrencyAPI;
+using CurrencyAPI.Models;
+using CurrencyAPI.Services.Interfaces;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyApiTests
+{
+    public class TestBase
+    {
+        protected private HttpClient _client { get; set; }
+        protected readonly ICurrencyService _currencyService;
+
+        public TestBase()
+        {
+            _client = new HttpClient
+            {
+                BaseAddress = new Uri("https://localhost:44399/"),
+                Timeout = new TimeSpan(0, 0, 0, 0, -1)
+            };
+        }
+
+        public CurrencyQuery createContent(string firstCurrency, string secondCurrency, string startDate, string endDate)
+        {
+            var queryFromData = new CurrencyQuery()
+            {
+                currencyCodes = new Dictionary<string, string>
+            {
+                { firstCurrency, secondCurrency} },
+                startDate = Convert.ToDateTime(startDate),
+                endDate = Convert.ToDateTime(endDate),
+                apiKey = "1dNE12qt2M9RSGzIWgaeqOYXoladM3GbVjXychLnR9w="
+            };
+
+
+            return queryFromData;
+
+        }
+
+
+    }
+}
